8ed95ad62cd13957c70ff4b111ff35a62d2ebdb0
Pchev78 <pchev78@yahoo.com>
2023-10-20 10:54:59 +0200

    Début Union Find

--- src/UnionFind.java
+++ src/UnionFind.java
@@ -2,9 +2,28 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class UnionFind<T>{
-    private Map<T,T> parent;
+    private Map<T,Integer> parent;
 
     public UnionFind() {
         parent = new HashMap<>();
+    }
+
+    public void mergeSet(UnionFind<T> element1, UnionFind<T> element2) {
+        Map<T,Integer> root1 = findParent(element1);
+        Map<T,Integer> root2 = findParent(element2);
+        if (root1 != root2) {
+            if (root1. < root2.) {
+                parent.put(root1,root2);
+                root2. += 1;
+            }
+            else {
+                parent.put(root2,root1);
+                root1. += 1;
+            }
+        }
+    }
+
+    private Map<T,Integer> findParent(UnionFind<T> element2) {
+
     }
 }
