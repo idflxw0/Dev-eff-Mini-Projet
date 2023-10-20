a91a37055aa9b6ef6f27c8fb0a71b2eac4106053
Kajanan <idfl0wxzero@gmail.com>
2023-10-20 12:03:24 +0200

    rank

--- src/UnionFind.java
+++ src/UnionFind.java
@@ -3,8 +3,11 @@ import java.util.Map;
 
 public class UnionFind<T>{
     private Map<T,T> parent;
+    private Map<T,Integer> rank;
 
     public UnionFind() {
         parent = new HashMap<>();
+        rank = new HashMap<>();
     }
+
 }
