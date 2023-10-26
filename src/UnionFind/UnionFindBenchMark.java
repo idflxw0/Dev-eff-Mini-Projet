528f6ba71a144b3f20c3def89ff423d2316e63a7
idfl0wx <idfl0wxzero@gmail.com>
2023-10-26 21:38:40 +0200

    extract output

--- src/UnionFind/UnionFindBenchMark.java
+++ src/UnionFind/UnionFindBenchMark.java
@@ -1,5 +1,8 @@
 package UnionFind;
 import UnionFind.UnionFind;
+
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -20,37 +23,47 @@ public class UnionFindBenchMark {
         return (System.nanoTime() - d) / 1E6;
     }
     public static void main(String[] args) {
-        List<Consumer<UnionFind<Integer>>> functions = Arrays.asList(
-                uf -> {
-                    for (int i = 0; i < MAX; i++) {
-                        uf.add(i);
-                    }
-                },
-                uf -> {
-                    for (int i = 0; i < MAX; i++) {
-                        uf.find(i);
-                    }
-                },
-                uf -> {
-                    for (int i = 1; i < MAX; i++) {
-                        uf.mergeSet(i, i - 1);
-                    }
-                }
-        );
+        try {
+            // Redirecting standard output to a file
+            PrintStream out = new PrintStream(new FileOutputStream("output.txt"));
+            System.setOut(out);
 
-        List<String> descriptions = Arrays.asList("add time: ", "find time: ", "merge time: ");
-        long startTime = System.nanoTime();
-        for (int n = MIN; n <= MAX; n += PAS) {
-            System.out.print(n + "\t");
-            UnionFind<Integer> unionFind = new UnionFind<Integer>();
-            for (int i = 0; i < functions.size(); i++) {
-                Consumer<UnionFind<Integer>> f = functions.get(i);
-                double time = getTime(f, unionFind);
-                System.out.print(descriptions.get(i) + time + "\t");
+            List<Consumer<UnionFind<Integer>>> functions = Arrays.asList(
+                    uf -> {
+                        for (int i = 0; i < MAX; i++) {
+                            uf.add(i);
+                        }
+                    },
+                    uf -> {
+                        for (int i = 0; i < MAX; i++) {
+                            uf.find(i);
+                        }
+                    },
+                    uf -> {
+                        for (int i = 1; i < MAX; i++) {
+                            uf.mergeSet(i, i - 1);
+                        }
+                    }
+            );
+
+            List<String> descriptions = Arrays.asList("add time: ", "find time: ", "merge time: ");
+            long startTime = System.nanoTime();
+            for (int n = MIN; n <= MAX; n += PAS) {
+                System.out.print(n + "\t");
+                UnionFind<Integer> unionFind = new UnionFind<Integer>();
+                for (int i = 0; i < functions.size(); i++) {
+                    Consumer<UnionFind<Integer>> f = functions.get(i);
+                    double time = getTime(f, unionFind);
+                    System.out.print(descriptions.get(i) + time + "\t");
+                }
+                System.out.println();
             }
-            System.out.println();
+            double totalTime = (System.nanoTime() - startTime) / 1E6;
+            System.out.println("Temps total pour terminer le programme: " + totalTime + " ms");
+
+            out.close(); // Close the output stream
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        double totalTime = (System.nanoTime() - startTime) / 1E6;
-        System.out.println("Temps total pour terminer le programme: " + totalTime + " ms");
     }
 }
